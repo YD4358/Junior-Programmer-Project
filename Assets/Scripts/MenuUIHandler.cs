f1e4df96db4342257c37f36f4b3556f3ba0a935c
Yash Dushettiwar <62443679+YD4358@users.noreply.github.com>
2022-06-04 15:32:31 +0530

    Changes

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -17,7 +17,7 @@ public class MenuUIHandler : MonoBehaviour
     public void Exit()
     {
         MainManager.Instance.SaveColor(); 
-        
+
         #if UNITY_EDITOR
                 EditorApplication.ExitPlaymode();
         #else
@@ -36,6 +36,17 @@ public class MenuUIHandler : MonoBehaviour
         //this will call the NewColorSelected function when the color picker have a color button clicked.
         ColorPicker.onColorChanged += NewColorSelected;
 
+        ColorPicker.SelectColor(MainManager.Instance.TeamColor);
+    }
+
+    public void SaveColorClicked()
+    {
+        MainManager.Instance.SaveColor();
+    }
+
+    public void LoadColorClicked()
+    {
+        MainManager.Instance.LoadColor();
         ColorPicker.SelectColor(MainManager.Instance.TeamColor);
     }
 }
